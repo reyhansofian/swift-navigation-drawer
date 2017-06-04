01449228c1c6d16a03e52fa3fa54a2d134efba68
Reyhan Sofian <computecoholic@gmail.com>
2017-06-04 11:13:03 +0700

    Initial Commit

---
+++ NavigationDrawer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationDrawer
+//
+//  Created by Reyhan Sofian on 6/4/17.
+//  Copyright © 2017 Reyhan Sofian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
